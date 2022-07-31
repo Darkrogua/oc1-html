daa528cabcd93f1812ffebd175939eb7ffd4fcc9
Артём Дмитренко <darkrogua@gmail.com>
2022-07-31 20:31:08 +0300

    добавил все остальные элементы

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
 $('button.navbar-toggler').on('click', function() {
    $('header').toggleClass('menu-open');
 });
+
+
+var swiper = new Swiper(".portfolioSlider", {
+   slidesPerView: "auto",
+   spaceBetween: 30,
+   pagination: false,
+   navigation: {
+      nextEl: '.slider-control__right',
+      prevEl: '.slider-control__left',
+   },
+});
